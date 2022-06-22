c6f403bcf1830e180afb4e92bc1cf6c998b0e841
rushmage <passionatemage@gmail.com>
2022-06-22 10:44:43 +0900

    1

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const http = require('http');
+const express = require("express");
+const server = express();
 
-http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('test');
-}).listen(24969);
-////test
-////111
-///1222
+server.use(express.static(__dirname + "/public"));
+
+server.get("/", (req, res) => {
+ 
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+
+server.use((req, res) => {
+  res.sendFile(__dirname + "/404.html");
+});
+
+server.listen(24969, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port number24***");
+});
